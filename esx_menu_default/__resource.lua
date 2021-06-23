9474f4eb0ab7f29b050cc99a11e39e509df51bec
IKER <63476973+IKER-36@users.noreply.github.com>
2021-06-23 12:03:24 +0200

    Update __resource.lua

--- esx_menu_default/__resource.lua
+++ esx_menu_default/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'MENU DEFAULT ESX'
 
-version '1.0.5'
+version '2.0'
 
 client_scripts {
 	'@es_extended/client/wrapper.lua',
